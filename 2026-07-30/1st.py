a4525dfcd85cd0ac9cc462f7aa186f346cb012ab
Sithija <sithijakalhara2@gmail.com>
2026-07-30 11:08:58 +0900

    Auto update at startup: 2026-07-30 11:08:57

---
+++ 2026-07-30/1st.py
@@ -0,0 +1,10 @@
+num1  = int(input("Enter first number: "))
+num2  = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+def addition(a, b, c):
+    total = a + b + c
+    print("Addition = ", total)
+
+
+addition(num1, num2, num3)
